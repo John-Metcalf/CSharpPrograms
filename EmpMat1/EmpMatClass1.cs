8607e9cb1768a6c3058da5f6ade4cca525fe36cc
John Metcalf <johnmetcalf777@gmail.com>
2020-08-17 21:04:42 -0400

    Here's the latest...

---
+++ EmpMat1/EmpMatClass1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FirstDataBase
+{
+    internal class EmpMatClass1
+    {
+        public EmpMatClass1(int ArrayValue)
+        {
+            string[] ApplicantDataBase = new string[ArrayValue];
+
+            for (int counter = 0; counter < ApplicantDataBase.Length; ++counter)
+                {
+                Console.WriteLine("Enter Name:");
+                string NameOfApplicant = Console.ReadLine();
+                ApplicantDataBase[counter] = NameOfApplicant;
+                }
+
+            Console.WriteLine();
+
+            for (int counter = 0; counter < ApplicantDataBase.Length; ++counter)
+            {
+                Console.WriteLine(ApplicantDataBase[counter]);
+            }
+        }
+    }
+}
---
+++ EmpMat1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FirstDataBase;
+
+namespace EmEmpMatClass1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Number Of Applicants:");
+            int ValueOfArray = Convert.ToInt32(Console.ReadLine());
+            EmpMatClass1 ThisIsTheName = new EmpMatClass1(ValueOfArray);
+        }
+
+    }
+}
